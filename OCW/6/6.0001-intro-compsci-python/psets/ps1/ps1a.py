c85a46f061547a5f9300b641e7d048e2ba20acc2
Jon Rodtang <jonrodtang@gmail.com>
2023-04-19 22:20:15 +0900

    Tried to improve ps1a but failed

--- OCW/6/6.0001-intro-compsci-python/psets/ps1/ps1a.py
+++ OCW/6/6.0001-intro-compsci-python/psets/ps1/ps1a.py
@@ -11,18 +11,22 @@ annual_salary = float(input("What is your annual salary in $?: "))
 portion_saved = float(input("How much of your salary do you want to save in %(0.0-1.0)?: "))
 total_cost    = float(input("What is the cost of your dream house in $?: "))
 
+current_savings = 0
 portion_down_payment = 0.25
-down_payment_cost = total_cost * portion_down_payment
 
 # annual returns
 r = 0.04
 
-current_savings = 0
+down_payment_cost = total_cost * portion_down_payment
+monthly_contribution = (annual_salary / 12) * portion_saved
+
 
 months = 0
+
 while current_savings < down_payment_cost:
-    months += 1
-    monthly_contribution = (annual_salary / 12) * portion_saved
-    monthly_returns = current_savings * (r / 12)     
+    months += 1       
+    monthly_returns = current_savings * (r / 12) 
     current_savings += monthly_returns + monthly_contribution
+
 print(months)
+
